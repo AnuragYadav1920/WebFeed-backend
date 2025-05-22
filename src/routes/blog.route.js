816c9962dc5700f6f76dd1a35707743dc31e6503
Anurag Yadav <anurag192000@gmail.com>
2025-05-22 20:20:55 +0530

    updated delete route

--- src/routes/blog.route.js
+++ src/routes/blog.route.js
@@ -6,7 +6,7 @@ const {verifyJWT} = require('../middlewares/auth.middleware.js')
 
 router.route("/create-post").post(verifyJWT,upload.single('postImage'),blogControllers.createPost)
 router.route("/update-post-details").put(verifyJWT,upload.single('postImage'),blogControllers.updatePostDetails)
-router.route("/delete-post").delete(verifyJWT,blogControllers.deletePost)
+router.route("/delete-post").post(verifyJWT,blogControllers.deletePost)
 router.route("/like-and-dislike").post(verifyJWT, blogControllers.likeAndDislike)
 router.route("/get-singlepost").post(blogControllers.getPostById)
 router.route("/get-all-posts").get(blogControllers.getAllPosts)
